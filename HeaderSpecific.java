a084dd68794db5bca653e3bbfa538bfdb274f066
Emporox-ch <amritchapagain007@gmail.com>
2025-07-27 23:19:44 +0545

    Add HeaderSpecific.java program

---
+++ HeaderSpecific.java
@@ -0,0 +1,10 @@
+import java.net.*;
+
+public class HeaderSpecific {
+    public static void main(String[] args) throws Exception {
+        URL url = new URL("https://www.google.com");
+        URLConnection con = url.openConnection();
+        System.out.println("Content-Type: " + con.getContentType());
+        System.out.println("Content-Length: " + con.getContentLength());
+    }
+}
