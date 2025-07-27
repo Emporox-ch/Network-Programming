4575ea4535dd165bd6c7614ac20d2e9fe0d959bc
Emporox-ch <amritchapagain007@gmail.com>
2025-07-27 23:19:46 +0545

    Add MACFromIP.java program

---
+++ MACFromIP.java
@@ -0,0 +1,13 @@
+
+import java.net.*;
+
+public class MACFromIP {
+    public static void main(String[] args) throws Exception {
+        InetAddress ip = InetAddress.getLocalHost();
+        NetworkInterface net = NetworkInterface.getByInetAddress(ip);
+        byte[] mac = net.getHardwareAddress();
+
+        System.out.print("MAC: ");
+        for (byte b : mac) System.out.printf("%02X-", b);
+    }
+}
