7ce99860f63e0c6aff58b590688058dfea7b30eb
Emporox-ch <amritchapagain007@gmail.com>
2025-07-27 23:19:42 +0545

    Add DownloadWebpageCharset.java program

---
+++ DownloadWebpageCharset.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.net.*;
+
+    public class DownloadWebpageCharset {
+    public static void main(String[] args) throws Exception {
+        URL url = new URL("https://www.google.com");
+        URLConnection con = url.openConnection();
+        String charset = con.getContentEncoding() != null ? con.getContentEncoding() : "UTF-8";
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream(), charset));
+        String line;
+        while ((line = in.readLine()) != null) System.out.println(line);
+    }
+}
