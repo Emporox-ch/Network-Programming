4ed09ab546d33acae6e8aac129a2497868cd84c3
Emporox-ch <amritchapagain007@gmail.com>
2025-07-27 23:20:07 +0545

    Add UDPOptions.java program

---
+++ UDPOptions.java
@@ -0,0 +1,12 @@
+import java.net.*;
+
+public class UDPOptions {
+    public static void main(String[] args) throws Exception {
+        DatagramSocket socket = new DatagramSocket();
+        socket.setReuseAddress(true);
+        socket.setSoTimeout(2000);
+        socket.setTrafficClass(0x10);
+        System.out.println("UDP options set.");
+        socket.close();
+    }
+}
