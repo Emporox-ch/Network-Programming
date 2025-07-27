eadb3cdaedc739dacf3de509a21bfe9ba8a37d17
Emporox-ch <amritchapagain007@gmail.com>
2025-07-27 23:19:47 +0545

    Add MulticastSender.java program

---
+++ MulticastSender.java
@@ -0,0 +1,12 @@
+import java.net.*;
+
+public class MulticastSender {
+    public static void main(String[] args) throws Exception {
+        InetAddress group = InetAddress.getByName("230.0.0.0");
+        MulticastSocket socket = new MulticastSocket();
+        String msg = "Hello Multicast";
+        DatagramPacket packet = new DatagramPacket(msg.getBytes(), msg.length(), group, 4446);
+        socket.send(packet);
+        socket.close();
+    }
+}
