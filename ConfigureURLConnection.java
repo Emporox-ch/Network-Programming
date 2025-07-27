74efc6d36d315a22d90345a5c42471ee6afd72e6
Emporox-ch <amritchapagain007@gmail.com>
2025-07-27 23:19:39 +0545

    Add ConfigureURLConnection.java program

---
+++ ConfigureURLConnection.java
@@ -0,0 +1,13 @@
+
+import java.net.*;
+
+public class ConfigureURLConnection {
+    public static void main(String[] args) throws Exception {
+        URL url = new URL("https://www.example.com");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setConnectTimeout(5000);
+        con.setReadTimeout(5000);
+        con.setRequestProperty("User-Agent", "Mozilla/5.0");
+        System.out.println("Configured URLConnection: " + con.getURL());
+    }
+}
