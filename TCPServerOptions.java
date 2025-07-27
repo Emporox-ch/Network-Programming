a0e1703735b7c7ed1441b769401a2ed0de7eee62
Emporox-ch <amritchapagain007@gmail.com>
2025-07-27 23:20:06 +0545

    Add TCPServerOptions.java program

---
+++ TCPServerOptions.java
@@ -0,0 +1,11 @@
+mport java.net.*;
+
+public class TCPServerOptions {
+    public static void main(String[] args) throws Exception {
+        ServerSocket server = new ServerSocket();
+        server.setReuseAddress(true);
+        server.bind(new InetSocketAddress(1234));
+        System.out.println("Server socket options set and bound.");
+        server.close();
+    }
+}
